20add8e438ec44c71bc06c413c508601fae2ffb5
Midhun <midhunsezhi@gmail.com>
2020-07-25 19:47:04 +0530

    add basic auth routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root 'landing#index'
+
+  devise_for :users, controllers: { omniauth_callbacks: 'oauths' }
+  devise_scope :user do
+    get '/login', to: 'devise/sessions#new', as: :new_session_path
+    get '/logout', to: 'devise/sessions#destroy', as: :destroy_session_path
+    get 'signup', to: 'devise/registrations#new', as: :signup
+  end
 end
