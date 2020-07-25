9240ff5e9b99c3df842898546a3517b22d02f019
Midhun <midhunsezhi@gmail.com>
2020-07-25 16:16:59 +0530

    add a simple landing controller and a root route

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,8 @@
+class LandingController < ApplicationController
+  def index
+    # if user_signed_in? && params[:lander].blank?
+    #   return redirect_to :home
+    # end
+    render layout: 'landing'
+  end
+end
