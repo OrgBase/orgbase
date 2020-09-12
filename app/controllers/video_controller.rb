08b8ceb315ee5e00b2637e6eae0acff42137e81e
Midhun Dandamudi <midhunsezhi@gmail.com>
2020-09-12 18:42:39 +0530

    add some guards around creating and joining a room.

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -2,12 +2,18 @@ class VideoController < ApplicationController
   before_action :authenticate_user!
 
   def join_room
+    @user = current_user
     room_name = params[:room]
     capacity = params[:capacity]
     client = TwilioService.client(room_name)
     room = client.video.rooms.list(unique_name: room_name).try(:first)
 
-    if room.present? && room.participants.list({status: 'connected'}).count >= capacity
+    employee_slug = @user.employee&.slug
+    participant_identity = "#{@user.first_name}-$-#{employee_slug}"
+    connected_participants = room&.participants&.list({status: 'connected'}) || []
+    already_connected = connected_participants.map(&:identity).include?(participant_identity)
+
+    if room.present? && !already_connected && connected_participants.count >= capacity
       return render json: {
           max_capacity: true
       }
@@ -15,7 +21,8 @@ class VideoController < ApplicationController
 
     # create a room if it doesn't exist
     room = TwilioService.p2p_room(client, room_name) if room.blank?
-    token = TwilioService.jwt_access_token(current_user.name, params[:room])
+
+    token = TwilioService.jwt_access_token(participant_identity, room_name)
 
     render json: {
         token: token,
