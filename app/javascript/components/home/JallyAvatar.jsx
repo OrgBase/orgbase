a6d7c3ee7ef76eb36324dd65d1a222842db192c4
Midhun Dandamudi <midhunsezhi@gmail.com>
2020-11-08 17:24:32 +0530

    add avatar dropdown

--- app/javascript/components/home/JallyAvatar.jsx
+++ app/javascript/components/home/JallyAvatar.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Avatar from "react-avatar";
 
 const JallyAvatar = ({ name, size, round }) => {
-  return <Avatar
-    name={name}
-    size={size}
-    round={round}
-    color='#FFFFFF'
-    fgColor='#FF3E8F'
-  />
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+
+  const toggleDropdown = () => setDropdownOpen(!dropdownOpen)
+
+  return <>
+    <div className="dropdown is-right is-active">
+      <div
+        className="dropdown-trigger"
+        onClick={toggleDropdown}
+      >
+        <Avatar
+          name={name}
+          size={size}
+          round={round}
+          color='#FFFFFF'
+          fgColor='#FF3E8F'
+        />
+      </div>
+      {dropdownOpen && <div className="dropdown-menu jally-dropdown-menu" role="menu">
+        <div className="dropdown-content">
+          <a href="#" className="dropdown-item">
+            Edit Profile
+          </a>
+          <a href="/logout" className="dropdown-item">
+            Sign Out
+          </a>
+        </div>
+      </div>}
+    </div>
+  </>
 }
 
 export default JallyAvatar;
