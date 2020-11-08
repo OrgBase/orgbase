2355e6a080906be89c8f985e10c4e4f88be716e3
Midhun Dandamudi <midhunsezhi@gmail.com>
2020-11-08 12:29:27 +0530

    Add styling for Lobby action cards

--- app/javascript/components/video/VideoChat.jsx
+++ app/javascript/components/video/VideoChat.jsx
@@ -19,53 +19,23 @@ const VideoChat = ({ name, errorMessage }) => {
         {errorMessage}
       </p>}
       <div>
-        <h2 className='title'>Hello, {name}! 👋 </h2>
-        <p className='mb-4 is-family-monospace subtitle'>Choose one of the options below to get started!</p>
-        <div className='columns ml-2 mr-2'>
-          <div className='column'>
+        <div className='columns is-centered ml-2 mr-2 '>
+          <div className='column is-narrow'>
             <div
-              className={`card jally-card ${roomLoading && 'pending'}`}
-              onClick={createAndRedirectToRoom}
-            >
-              <div className="card-header">
-                <div className="card-header-title is-centered">
-                  <span>Enter a game room.</span>
-                </div>
-              </div>
-              <div className="card-content">
-                A game room can accommodate upto 4 people including yourself! You'll get the link to invite
-                others once you're in the room.
-              </div>
-            </div>
-          </div>
-          <div className='column'>
-            <div
-              className={`card jally-card ${sessionLoading && 'pending'}`}
+              className={`jally-card start-card ${sessionLoading && 'pending'}`}
               onClick={createAndRedirectToSession}
             >
-              <div className="card-header">
-                <div className="card-header-title is-centered">
-                  <span>Start a Jally!</span>
-                </div>
-              </div>
               <div className="card-content">
-                A Jally is a bonding session for multiple people to join. Users will be randomly paired up and added to
-                separate game rooms as they join the session.
-                You'll get the link to share the session in the next step.
+                <div className='jally-card-header mb-2'>START</div>
+                <div className='jally-card-text'>a Jally now</div>
               </div>
             </div>
           </div>
-          <div className='column'>
-            <div className="card jally-card">
-              <div className="card-header">
-                <div className="card-header-title is-centered">
-                  <span>Schedule a Jally for later.</span>
-                </div>
-              </div>
+          <div className='column is-narrow'>
+            <div className="jally-card schedule-card">
               <div className="card-content">
-                Use this option to schedule and configure a recurring or a one-time Jally
-                for your team to participate in the future.
-                You can also choose to send emails with calendar invites to all team members.
+                <div className='jally-card-header mb-2'>SCHEDULE</div>
+                <div className='jally-card-text'>a Jally for later</div>
               </div>
             </div>
           </div>
