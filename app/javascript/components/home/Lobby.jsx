e3c9ebbf1400a48cc3fb1bfc83c4448159248bae
Midhun Dandamudi <midhunsezhi@gmail.com>
2020-11-22 21:42:10 +0530

    refactor Lobby component

--- app/javascript/components/home/Lobby.jsx
+++ app/javascript/components/home/Lobby.jsx
@@ -1,12 +1,71 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import VideoChat from "../video/VideoChat";
+import Modal from "../common/modal";
+import CreateJallyForm from "./CreateJallyForm";
 
-const Lobby = props => {
+const Lobby = ({ errorMessage, notice }) => {
+  const [startModalState, setStartModalState] = useState(false)
+  const [scheduleModalState, setScheduleModalState] = useState(false)
+
+  const toggleStartModal = () => setStartModalState(!startModalState)
+  const toggleScheduleModal = () => setScheduleModalState(!scheduleModalState)
+
   return (
     <>
-        <VideoChat {...props}/>
+      <div className="has-text-centered mt-6">
+        {errorMessage && <p className="notification is-warning mx-6">
+          {errorMessage}
+        </p>}
+        {notice && <p className="notification is-success mx-6">
+          {notice}
+        </p>}
+        <div>
+          <div className='columns is-centered ml-2 mr-2'>
+            <div className='column is-narrow'>
+              <div
+                className='jally-card start-card'
+                onClick={toggleStartModal}
+              >
+                <div className="card-content">
+                  <div className='jally-card-header mb-2'>START</div>
+                  <div className='jally-card-text'>a Jally now</div>
+                </div>
+              </div>
+            </div>
+            <div className='column is-narrow'>
+              <div
+                className="jally-card schedule-card"
+                onClick={toggleScheduleModal}
+              >
+                <div className="card-content">
+                  <div className='jally-card-header mb-2'>SCHEDULE</div>
+                  <div className='jally-card-text'>a Jally for later</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Modal
+        modalState={scheduleModalState}
+        modalTitle='Schedule a Jally'
+        closeModal={toggleScheduleModal}
+      >
+        <CreateJallyForm
+          isImpromptu={false}
+        />
+      </Modal>
+      <Modal
+        modalState={startModalState}
+        closeModal={toggleStartModal}
+        modalTitle='Start a Jally'
+        className='jally-modal'
+      >
+        <CreateJallyForm
+          isImpromptu={true}
+        />
+      </Modal>
     </>
   );
 };
