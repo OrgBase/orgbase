f1ae04b02be043f4af89ce4831af72f6a1a4f960
Midhun Dandamudi <midhunsezhi@gmail.com>
2021-02-21 23:28:39 +0530

    move game title and instructions button to the top of panel

--- app/javascript/components/video/SidePanel.jsx
+++ app/javascript/components/video/SidePanel.jsx
@@ -158,14 +158,26 @@ const SidePanel = ({ localParticipant, roomName, syncGameData, participantIdenti
       {type == 'ice-breaker' && <img className='game-type-logo' src={iceBreakerLogo}/>}
       {type != 'ice-breaker' && <img className='game-type-logo' src={gameLogo}/>}
 
-      <h3 className="game-title has-text-white"> { title } </h3>
+      <div className="panel-header">
+        <h3 className="game-title has-text-white align-start"> { title } </h3>
+        {showInstructions ? <button
+          className='jally-button-small transparent-button my-3'
+          onClick={toggleInstructions}
+        >
+          Back
+        </button> : <button
+          className='jally-button-small transparent-button my-3'
+          onClick={toggleInstructions}
+        >
+          Show Instructions
+        </button>}
+      </div>
+
       {showInstructions ? <>
-        <button className='jally-button-small transparent-button my-3' onClick={toggleInstructions}>Back</button>
         <div className="game-rules px-2 mb-2">
           {instructions}
         </div>
       </> : <>
-        <button className='jally-button-small transparent-button my-3' onClick={toggleInstructions}>Show Instructions</button>
         {(type == 'ice-breaker' || isActiveParticipant()) ? renderVariant()  :
           <div className="game-variant px-2 mb-2">
             {renderPassiveParticipantArea()}
