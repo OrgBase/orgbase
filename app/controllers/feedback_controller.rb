6c36e2584eaff4767264678f8dbf3ddf8d756aff
Midhun Dandamudi <midhunsezhi@gmail.com>
2021-05-11 19:14:26 +0530

    submit feedback to slack

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -9,7 +9,18 @@ class FeedbackController < ApplicationController
     user = current_user
     employee = user&.employee
 
-    Feedback.create!(employee: employee, feedback_type: feedback_type, score: score.to_i)
+    Feedback.create!(employee: employee, feedback_type: feedback_type, score: score&.to_i)
+
+    message = "New feedback from #{user.name}(#{user.email}) of #{employee.company.name} : "
+
+    if feedback_type == 'recommend'
+      message += "Likehood to recommend to others: #{score}/10"
+    else
+      feelings = ["", "Not Disappointed", "Somewhat disappointed", "Very disappointed"]
+      message += "Feeling if they can't use jally: #{feelings[score&.to_i]}"
+    end
+
+    Slack.send_message("#user-activity", message)
 
     redirect_to root_path
   end
